b424f7e8246d09230ac5f61e28c6d601a486bbbf
SulpisiusBernikustiMentik <sulpisiusmentik@gmail.com>
2023-05-02 15:22:20 +0800

    menambahkan sorting dan memperbaiki menambahkan produk ke keranjang

--- shop.php
+++ shop.php
@@ -10,6 +10,24 @@ if(!isset($user_id)){
    header('location:login.php');
 }
 
+if(isset($_POST['add_to_cart'])){
+
+   $product_name = $_POST['product_name'];
+   $product_price = $_POST['product_price'];
+   $product_image = $_POST['product_image'];
+   $product_quantity = $_POST['product_quantity'];
+
+   $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+
+   if(mysqli_num_rows($check_cart_numbers) > 0){
+      $message[] = 'already added to cart!';
+   }else{
+      mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, quantity, image) VALUES('$user_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
+      $message[] = 'product added to cart!';
+   }
+
+}
+
 $sortby = 'name';
 $sorttype = 'asc';
 
