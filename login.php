c65b26d392f8d34d20e0ecd70a1e9c5c07f2b39e
SulpisiusBernikustiMentik <sulpisiusmentik@gmail.com>
2023-05-03 00:10:26 +0800

    menambahkan role employee

--- login.php
+++ login.php
@@ -21,6 +21,16 @@ if(isset($_POST['submit'])){
          $_SESSION['admin_id'] = $row['id'];
          header('location:admin_page.php');
 
+      }elseif($row['user_type'] == 'employee'){
+
+         $_SESSION['user_name'] = $row['name'];
+         $_SESSION['user_email'] = $row['email'];
+         $_SESSION['user_id'] = $row['id'];
+         header('location:employee/admin_users.php');
+
+      }
+
+
       }elseif($row['user_type'] == 'user'){
 
          $_SESSION['user_name'] = $row['name'];
@@ -34,7 +44,7 @@ if(isset($_POST['submit'])){
       $message[] = 'incorrect email or password!';
    }
 
-}
+
 
 ?>
 
