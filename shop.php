8216fe7fa83f27a8b1dc90bcb6bf5aa2e100d9e1
SulpisiusBernikustiMentik <sulpisiusmentik@gmail.com>
2023-05-02 15:05:40 +0800

    menambahkan sorting pada file shop.php

--- shop.php
+++ shop.php
@@ -10,22 +10,25 @@ if(!isset($user_id)){
    header('location:login.php');
 }
 
-if(isset($_POST['add_to_cart'])){
+$sortby = 'name';
+$sorttype = 'asc';
 
-   $product_name = $_POST['product_name'];
-   $product_price = $_POST['product_price'];
-   $product_image = $_POST['product_image'];
-   $product_quantity = $_POST['product_quantity'];
+// jika ada request sorting dari pengguna
+if(isset($_GET['sort'])){
+    $sortby = $_GET['sortby'];
+    $sorttype = $_GET['sorttype'];
+}
 
-   $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+// query untuk mengambil data produk dari database dengan urutan yang diinginkan
+$select_products = mysqli_query($conn, "SELECT * FROM `products` ORDER BY $sortby $sorttype") or die('query failed');
 
-   if(mysqli_num_rows($check_cart_numbers) > 0){
-      $message[] = 'already added to cart!';
-   }else{
-      mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, quantity, image) VALUES('$user_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
-      $message[] = 'product added to cart!';
-   }
-
+// tampilkan data produk yang sudah diurutkan
+if(mysqli_num_rows($select_products) > 0){
+    while($fetch_products = mysqli_fetch_assoc($select_products)){
+        // kode untuk menampilkan data produk
+    }
+} else {
+    echo '<p class="empty">tidak ada produk ditambahkan!</p>';
 }
 
 ?>
@@ -45,27 +48,63 @@ if(isset($_POST['add_to_cart'])){
    <link rel="stylesheet" href="css/style.css">
 
 </head>
+<style>
+   label 
+{font-size: 20px
+
+}
+</style>
 <body>
    
 <?php include 'header.php'; ?>
 
 <div class="heading">
    <h3>our shop</h3>
-   <p> <a href="home.php">home</a> / shop </p>
+   <p> <a href="index.php">home</a> / shop </p>
 </div>
 
 <section class="products">
 
    <h1 class="title">Produk terbaru</h1>
-
+   <form action="" method="get" style="margin-left: 900px;">
+    <label for="sortby">Urutkan berdasarkan:</label>
+    <select name="sortby" id="sortby">
+        <option value="name" <?php if($sortby == 'name') echo 'selected'; ?>>Nama</option>
+        <option value="price" <?php if($sortby == 'price') echo 'selected'; ?>>Harga</option>
+    </select>
+    <select name="sorttype" id="sorttype">
+        <option value="asc" <?php if($sorttype == 'asc') echo 'selected'; ?>>Ascending</option>
+        <option value="desc" <?php if($sorttype == 'desc') echo 'selected'; ?>>Descending</option>
+    </select>
+    <button type="submit" name="sort">Urutkan</button>
+</form>
+<br>
+<br>
    <div class="box-container">
+<!-- form untuk memilih opsi sorting -->
 
-      <?php  
-         $select_products = mysqli_query($conn, "SELECT * FROM `products`") or die('query failed');
-         if(mysqli_num_rows($select_products) > 0){
-            while($fetch_products = mysqli_fetch_assoc($select_products)){
-      ?>
-     <form action="" method="post" class="box">
+ 
+<?php
+   // cek apakah ada request untuk melakukan sorting
+if(isset($_GET['sort'])){
+   $sortby = $_GET['sortby'];
+   $sorttype = $_GET['sorttype'];
+
+   // query untuk mengambil data dari tabel products dan diurutkan berdasarkan kolom yang dipilih
+   $query = "SELECT * FROM `products` ORDER BY `$sortby` $sorttype";
+}else{
+   // jika tidak ada request sorting, tampilkan semua data produk tanpa sorting
+   $query = "SELECT * FROM `products`";
+}
+
+// eksekusi query
+$select_products = mysqli_query($conn, $query) or die('query failed');
+
+// tampilkan data produk
+if(mysqli_num_rows($select_products) > 0){
+   while($fetch_products = mysqli_fetch_assoc($select_products)){
+?>
+   <form action="" method="post" class="box">
       <img class="image" src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
       <div class="name"><?php echo $fetch_products['name']; ?></div>
       <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
@@ -74,13 +113,14 @@ if(isset($_POST['add_to_cart'])){
       <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
       <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
       <input type="submit" value="add to cart" name="add_to_cart" class="btn">
-     </form>
-      <?php
-         }
-      }else{
-         echo '<p class="empty">tidak ada produk ditambahkan!</p>';
-      }
-      ?>
+   </form>
+<?php
+   }
+}else{
+   echo '<p class="empty">tidak ada produk ditambahkan!</p>';
+}
+?>
+
    </div>
 
 </section>
